74314dd6da59f775c5fc26b25ccd38d3b4af36f9
aeremenok <eav1986@gmail.com>
2011-07-23 00:10:15 +0400

    initial project structure

---
+++ jbpm-examples/jbpm-jpdl/src/main/java/edu/leti/jbpm/domain/Product.java
@@ -0,0 +1,5 @@
+package edu.leti.jbpm.domain;
+
+/** @author eav Date: 23.07.11 Time: 0:07 */
+public class Product {
+}
---
+++ jbpm-examples/jbpm-jpdl/src/test/java/edu/leti/jbpm/ProcessTest.java
@@ -0,0 +1,5 @@
+package edu.leti.jbpm;
+
+/** @author eav Date: 23.07.11 Time: 0:08 */
+public class ProcessTest {
+}
