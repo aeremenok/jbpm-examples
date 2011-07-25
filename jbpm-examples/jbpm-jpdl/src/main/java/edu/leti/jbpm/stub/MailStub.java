2b6fadd0c916774f809e0f31b01e898821670ae8
aeremenok <eav1986@gmail.com>
2011-07-25 21:25:01 +0400

    mail "sending" configured

---
+++ jbpm-examples/jbpm-jpdl/src/main/java/edu/leti/jbpm/stub/MailStub.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package edu.leti.jbpm.stub;
+
+import org.apache.log4j.Logger;
+import org.jbpm.mail.Mail;
+
+/**
+ * @author eav 2011
+ */
+public class MailStub extends Mail {
+    private static final Logger log = Logger.getLogger( MailStub.class );
+
+    public MailStub() {
+        super();
+    }
+
+    public MailStub( final String template,
+                     final String actors,
+                     final String to,
+                     final String bccActors,
+                     final String bcc,
+                     final String subject,
+                     final String text ) {
+        super( template, actors, to, bccActors, bcc, subject, text );
+    }
+
+    public MailStub( final String template,
+                     final String actors,
+                     final String to,
+                     final String subject,
+                     final String text ) {
+        super( template, actors, to, subject, text );
+    }
+
+    @Override
+    public void send() {
+        try {
+            super.send();
+        } catch ( final Throwable e ) {
+            log.error( e, e );
+        }
+    }
+}
